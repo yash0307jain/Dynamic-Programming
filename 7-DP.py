b8468a971aba519693cf2914b24829e312796573
Yash Jain <yash0307jain@gmail.com>
2019-06-27 23:05:06 +0530

    subset subsequence optimizes with dp

---
+++ 7-DP.py
@@ -0,0 +1,45 @@
+# Subset Subsequence
+print("----------INPUT----------")
+
+n, num = input().split(' ')
+n = int(n)
+num = int(num)
+str_arr = input().split(' ')
+arr = [int(num) for num in str_arr]
+
+# print("----------DP----------")
+
+def ss(arr, num, ind, mem, count, memory):
+    key = str(ind) + ":" + str(mem)
+
+    if mem > num:
+        return count
+
+    if key in memory:
+        count.append(1)
+        return memory[key]
+        
+    print("ind --> ", ind, " mem --> ", mem)
+
+    if ind < 0:
+        memory[key] = mem
+        return count.append(1)
+    
+    ss(arr, num, ind - 1, mem * arr[ind], count, memory)
+    ss(arr, num, ind - 1, mem, count, memory)
+
+def toSS(arr, num):
+    mem = 1
+    count = []
+    memory = {}
+    ss(arr, num, len(arr) - 1, mem, count, memory)
+    return count
+
+print("----------OUTPUT----------")
+
+arr = sorted(arr, reverse = True)
+
+result = toSS(arr, num)
+print(len(result) - 1)
+
+print("----------END----------")
